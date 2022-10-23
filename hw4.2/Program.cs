243c12e08e28bbf9615538bfd4b48818b8d55035
IvanZakharov <blackmore09218@yandex.ru>
2022-10-23 21:53:29 +0300

    Сумма цифр числа

---
+++ hw4.2/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+Console.WriteLine("Введите число:");
+string NumberStr = Console.ReadLine();
+int Sum = 0;
+for (int i = 0; i < NumberStr.Length; i++)
+{
+    string buffer = NumberStr[i].ToString();
+    int CurrentDigit = int.Parse(buffer);
+    Sum+=CurrentDigit;
+}
+Console.WriteLine("Сумма цифр введенного числа - "+Sum);
