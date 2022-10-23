6c2540ca13e85874b0df2ed88c3eab5e3ac734e7
IvanZakharov <blackmore09218@yandex.ru>
2022-10-23 22:51:43 +0300

    Вывод элементов массива

---
+++ hw4.3/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+пример из 5 элементов:
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+получить все 8 чисел за одно нажатие Enter от пользователя.подсказка: использовать метод Split();*/
+Console.WriteLine("Введите элементы массива, разделяя их одним пробелом");
+string NumbersStr = Console.ReadLine();
+string[] array = NumbersStr.Split();
+Console.Write("[");
+for (int i = 0; i < array.Length-1; i++) Console.Write(array[i]+", ");
+Console.Write(array[array.Length-1]+"]");
