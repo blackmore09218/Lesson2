fc63c8089e113c193438493c953bc07bc85e09ee
IvanZakharov <blackmore09218@yandex.ru>
2022-10-23 20:08:06 +0300

    Цикл по возведению в степень

---
+++ hw4.1/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+Console.WriteLine("Введите основание, число А:");
+double numberA = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите показатель степени, число В:");
+int numberB = int.Parse(Console.ReadLine());
+double result = 1;
+for (int i = 1; i <= numberB; i++) result *= numberA;
+Console.WriteLine("Результат  "+result);
