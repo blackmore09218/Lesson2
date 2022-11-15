6c28ee23f3996a086d3baefe2afeefff11667931
IvanZakharov <blackmore09218@yandex.ru>
2022-11-15 20:52:55 +0300

    41

---
+++ seminar6/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел > 0 ввел пользователь*/
+Console.WriteLine("Введите количество чисел, которые собираетесь ввести:   ");
+int M = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i=1; i<M+1; i++)
+{
+    Console.WriteLine($"Введите очередное число. Количество чисел, которое осталось ввести: {M-i+1}");
+    int CurrentNumber = int.Parse(Console.ReadLine());
+    if (CurrentNumber > 0) count+=1;
+}
+Console.WriteLine($"Количество введенных положительных чисел: {count}");
